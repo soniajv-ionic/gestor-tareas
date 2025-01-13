2f30046bf8928fc3b9ddf7cac9aecbbd8bba9212
soniajv-ionic <soniajv.ionic@gmail.com>
2025-01-13 23:33:11 +0100

    update component ficha-tarea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FichaTareaComponent } from './ficha-tarea/ficha-tarea.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FichaTareaComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'gestor-tareas';
---
+++ src/app/ficha-tarea/ficha-tarea.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ficha-tarea',
+  imports: [],
+  templateUrl: './ficha-tarea.component.html',
+  styleUrl: './ficha-tarea.component.css'
+})
+export class FichaTareaComponent {
+
+}
